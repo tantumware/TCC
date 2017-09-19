cd22ea74f13537f83986a71b83a3582dc49574cb
pedro <pedropacheco92@outlook.com>
2017-09-19 17:04:17 -0300

    algoritmo

--- tcc-app/ionic/tantum/src/models/algoritmo.js
+++ tcc-app/ionic/tantum/src/models/algoritmo.js
@@ -1,7 +1,7 @@
 var Stream = require('streamjs');
 
 //private Dictionary<string, List<string>> Disciplinas;
-var disciplinas = [];
+// var disciplinas = [];
 
 //public Dictionary<string, int> Pontos {get; set; }
 var pontos = [];
@@ -27,10 +27,10 @@ function createCurriculo(listaDisciplinas) {
         Stream(curriculo)
             .filter(d => d.key == i)
             .forEach(d => {
-                disciplinas.push({
-                    key: d.value.key, 
-                    value: d.value.value
-                })
+                // disciplinas.push({
+                //     key: d.value.key, 
+                //     value: d.value.value
+                // })
 
                 if (Stream(pontos).filter(p => p.key == d.value.key).findAny().isPresent()) {
                     Stream(pontos).filter(p => p.key == d.value.key).findAny().get().rank++;
@@ -127,119 +127,3 @@ var x = [
 ];
 
 rank(x);
-
-
- /*  public DisciplinaHelper(List < Disciplina > disciplinas)
-{
-    // recebe a lista de disciplinas do curso e monta do map
-    curriculo = new Dictionary<int, Dictionary<string, List<string>>>();
-
-    disciplinas.ForEach(d => {
-        if (!curriculo.ContainsKey(d.Fase)) {
-            curriculo.Add(d.Fase, new Dictionary<string, List<string>>());
-        }
-        curriculo[d.Fase].Add(d.Codigo, d.Requisitos);
-    });
-}
-
-        public void RankDisciplinas()
-{
-    Pontos = new Dictionary<string, int>();
-    Disciplinas = new Dictionary<string, List<string>>();
-
-    // para cada materia da fase, começando pela ultima
-    for (int i = curriculo.Count; i >= 1; i--)
-    {
-        foreach(KeyValuePair < string, List < string >> entry in curriculo[i])
-        {
-            // cria estrututra que contém todas as disciplinas e seus requisitos
-            Disciplinas.Add(entry.Key, entry.Value);
-
-            // controle da disciplina
-            if (Pontos.ContainsKey(entry.Key)) {
-                // se ja existe a disciplina, soma 1
-                Pontos[entry.Key] += 1;
-            }
-            else {
-                // senao adiciona com valor 1
-                Pontos.Add(entry.Key, 1);
-            }
-
-            // controle dos requisitos
-            foreach(string requisito in entry.Value)
-            {
-                if (Pontos.ContainsKey(requisito)) {
-                    // se ja existe o requisito, soma 1
-                    Pontos[requisito] += 1;
-                }
-                else {
-                    // senao passa o valor da materia
-                    Pontos.Add(requisito, Pontos[entry.Key]);
-                }
-
-            }
-        }
-
-    }
-}
-
-        public List < string > Compute(List < string > disciplinasCursadas, List < string > disciplinasDesejadas)
-{
-    List < string > result = new List<string>();
-
-    Dictionary < string, int > possiveisDisciplinas = new Dictionary<string, int>();
-
-    // cria o a lista de materias que nao foram cursadas ordenadas por pontos
-    foreach(KeyValuePair < string, int > entry in Pontos)
-    {
-        // se ele ja cursou, não adiciona na lista de possiveis materias
-        if (!disciplinasCursadas.Contains(entry.Key)) {
-            // se nao tem requisitos, adiciona na lista de possiveis
-            if (Disciplinas[entry.Key].Count == 0) {
-                possiveisDisciplinas.Add(entry.Key, entry.Value);
-            }
-            // senao, verifica se ja cursou os requisitos
-            else {
-                bool todosRequisitos = true;
-                foreach(string req in Disciplinas[entry.Key])
-                {
-                    // se nao tiver cursado o requisito, seta todos os requisitos como false
-                    if (!disciplinasCursadas.Contains(req)) {
-                        todosRequisitos = false;
-                        break;
-                    }
-                }
-
-                // se tiver todos os requisitos adiciona na lista de possiveis disciplinas
-                if (todosRequisitos) {
-                    possiveisDisciplinas.Add(entry.Key, entry.Value);
-                }
-            }
-
-        }
-    }
-
-    int i = 0;
-    // ordena a lista por peso
-    possiveisDisciplinas.OrderBy(e => e.Value);
-    foreach(KeyValuePair < string, int > entry in possiveisDisciplinas)
-    {
-        // TODO falta metodo pra saber todas as materias, com o horario, se tem choque de horário
-        // e falta considerar disciplinas desejadas
-        if (i == 3) {
-            break;
-        }
-
-        result.Add(entry.Key);
-
-        i++;
-    }
-
-
-    return result;
-}
-
-
-    }
-}
-*/
