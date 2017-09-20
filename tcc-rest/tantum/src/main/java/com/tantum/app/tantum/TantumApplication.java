90166bc4ecae0c1376557e39ad4d486010a4ec49
pedro <pedropacheco92@outlook.com>
2017-09-20 20:46:25 -0300

    rest

---
+++ tcc-rest/tantum/src/main/java/com/tantum/app/tantum/TantumApplication.java
@@ -0,0 +1,12 @@
+package com.tantum.app.tantum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TantumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TantumApplication.class, args);
+	}
+}
