02f6df5a9ea0df2af62f323560d0d58436438cbf
Pedro <pedropacheco92@outlook.com>
2018-01-19 17:13:59 -0200

    wip

--- tcc-app/ionic/tantum/src/pages/main/main.ts
+++ tcc-app/ionic/tantum/src/pages/main/main.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MainPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-main',
@@ -27,7 +20,7 @@ export class MainPage {
   }
 
   onSairClicked(): void {
-    
+    this.navCtrl.pop();
   }
 
 }
