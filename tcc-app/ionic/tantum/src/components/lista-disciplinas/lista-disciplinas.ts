30623c4a7dea793b5d9e7722d6df75255477d693
Pedro <pedropacheco92@outlook.com>
2018-02-01 17:37:28 -0200

    lista disciplinas wip

--- tcc-app/ionic/tantum/src/components/lista-disciplinas/lista-disciplinas.ts
+++ tcc-app/ionic/tantum/src/components/lista-disciplinas/lista-disciplinas.ts
@@ -1,3 +1,4 @@
+import { AlertController } from 'ionic-angular';
 import { Disciplina } from './../../models/disciplina';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
@@ -11,14 +12,25 @@ export class ListaDisciplinasComponent {
 
   @Output() onSwiped = new EventEmitter<boolean>();
 
-  constructor() {
+  constructor(private alertCtrl: AlertController) {
   }
 
   itemSwiped(s: any) {
-    console.log(s.direction);
     if (s.direction == 2) { // 4 left
       this.onSwiped.emit(true);
+    } else {
+      this.onSwiped.emit(false);
     }
+  }
+
+  onDisciplinaClicked(disciplina: Disciplina): void {
+    console.log(disciplina);
+    let alert = this.alertCtrl.create({
+      title: 'Detalhes da disciplina',
+      subTitle: disciplina.nome,
+      buttons: ['Voltar']
+    });
+    alert.present();
   }
 
 }
--- tcc-app/ionic/tantum/src/pages/grade-horarios/grade-horarios.ts
+++ tcc-app/ionic/tantum/src/pages/grade-horarios/grade-horarios.ts
@@ -1,5 +1,6 @@
+import { Dia } from './../../models/dia';
 import { Disciplina } from './../../models/disciplina';
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterContentChecked } from '@angular/core';
 import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 
 @IonicPage()
@@ -9,28 +10,73 @@ import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
 })
 export class GradeHorariosPage {
 
-  passo: string;
+  private passo: string;
+
+  private dia: number;
+
+  private dias: Dia[];
 
   @ViewChild('slides') slides: Slides;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.dia = new Date().getDay() - 1; // começa na segunda
+    console.log(this.dia);
+    this.dias = Dia.getAllDias();
   }
 
   ionViewDidLoad() {
     this.passo = '1';
   }
-
+  
   onPassoChanged(): void {
+    console.log('passo');
+  }
 
+  onSlideChanged(event: any): void {
+    this.dia = event._activeIndex;
   }
 
   onSwiped(event: boolean): void {
-    console.log(event);
-    this.slides.slideTo(1, 500);
+    console.log("swipe");
+    if (event) {
+      this.swipeRight();
+    } else {
+      this.swipeLeft(); 
+    }
+  }
+  
+  swipeRight(): void {
+    this.dia = this.dia + 1;
+    
+    console.log('right'+this.dia);
+    this.slides.slideTo(this.dia);
+  }
+
+  swipeLeft(): void {
+    this.dia = this.dia - 1;
+    
+    console.log('left'+this.dia);
+    this.slides.slideTo(this.dia);
+  }
+
+  onDiaClicked(dia: Dia): void {
+    this.dia = this.dias.indexOf(dia);
+    this.slides.slideTo(this.dia);
   }
 
   getDisciplinas(): Disciplina[] {
     return [new Disciplina('Disciplina 1'), new Disciplina('Disciplina 2'), new Disciplina('Disciplina 3')]
+  }
+
+  getClass(dia: Dia): string {
+    let clazz: string = "";
+    if (!dia.visible) {
+      clazz += "dia-hidden ";
+    }
+    if (this.dias[this.dia].nomeAbreviado == dia.nomeAbreviado){
+      clazz += "dia-selected";
+    }
+    return clazz;
   }
 
 }
