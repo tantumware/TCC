4f1aa822527840b44e70b146db76ae0293333fe7
Pedro <pedropacheco92@outlook.com>
2018-02-02 16:46:15 -0200

    wip

--- tcc-app/ionic/tantum/src/models/token.ts
+++ tcc-app/ionic/tantum/src/models/token.ts
@@ -1,24 +1,24 @@
 import {Buffer} from 'buffer';
+import {Hashids} from 'hashids';
 
 declare var System: any;
 
 export class TokenHelper {
 
     static encodeToken(plainMessage: string): any {
+        var hashids = new Hashids("this is my salt", 16);
         let aux;
-        let hex = Buffer(plainMessage).toString('hex');
-        System.import('hashids')
-            .then(x => aux = x.encodeHex(hex));
+        let hex = Buffer.from(plainMessage).toString('hex');
+        // aux = hash.encodeHex(hex)
 
         return aux;
     }
 
     static decodeToken(hashedMessage: string): any {
         let decodedHex;
-        System.import('hashids')
-            .then(x => decodedHex = x.decodeHex(hashedMessage));
+        // decodedHex = Buffer.from(hash.decodeHex(hashedMessage), 'hex').toString('utf8')
 
-        return Buffer(decodedHex, 'hex').toString('utf8');;
+        return decodedHex;
     }
 
 }
