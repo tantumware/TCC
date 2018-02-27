e2d8424cae3dc92d709b51a323fe323d2a653159
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-02-27 11:53:30 -0300

    new main

--- tcc-app/ionic/tantum/src/pages/main/main.module.ts
+++ tcc-app/ionic/tantum/src/pages/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MainPage } from './main';
 import { TranslateModule } from '@ngx-translate/core';
+import { ListaDisciplinasComponent } from '../../components/lista-disciplinas/lista-disciplinas';
 
 @NgModule({
   declarations: [
