941e1bab8b53a39a84b5ee64dd4c8b493b6202d6
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-02-27 16:26:17 -0300

    wip

--- tcc-app/ionic/tantum/src/utils/formatter.ts
+++ tcc-app/ionic/tantum/src/utils/formatter.ts
@@ -4,8 +4,8 @@ export class FormatterUtils {
     }
 
     /**
-     * Method to format de hour from this pattern: d.hh:mm-n and return hh:mm
-     * @param value example 2.08:20-2 and return 08:20
+     * Method to format de hour from this pattern: d.hhmm-n and return hh:mm
+     * @param value example 2.0820-2 and return 08:20
      */
     public static formatHour(value: string) {
         let hour = value.substring(2, 6);
