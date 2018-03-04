d8f75d2b1e8bb0ef8d7ea0d58311630bbe53a207
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-03-04 18:53:07 -0300

    wip

--- tcc-app/ionic/tantum/src/components/disciplina-criterio/disciplina-criterio.ts
+++ tcc-app/ionic/tantum/src/components/disciplina-criterio/disciplina-criterio.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-/**
- * Generated class for the DisciplinaCriterioComponent component.
- *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
- */
 @Component({
   selector: 'disciplina-criterio',
   templateUrl: 'disciplina-criterio.html'
