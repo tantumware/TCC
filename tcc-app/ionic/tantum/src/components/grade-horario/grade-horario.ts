21ef141a4803d91f94e115d09dc45b84879e6318
Pedro Henrique Pacheco <pedropacheco92@outlook.com>
2018-03-06 14:23:01 -0300

    grade horario

--- tcc-app/ionic/tantum/src/components/grade-horario/grade-horario.ts
+++ tcc-app/ionic/tantum/src/components/grade-horario/grade-horario.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import { DisciplinaListItem } from './../../models/disciplia-list-item';
 import { Component, Input } from '@angular/core';
 import { AlertController } from 'ionic-angular';
@@ -10,21 +11,48 @@ export class GradeHorarioComponent {
 
   @Input() disciplinas: DisciplinaListItem[];
 
-  constructor(private alertCtrl: AlertController) {
+  private afternoon: string;
+  private morning: string;
+  private night: string;
+
+  private monday: string;
+  private tuesday: string;
+  private wednesday: string;
+  private thursday: string;
+  private friday: string;
+
+  constructor(private alertCtrl: AlertController, private translate: TranslateService) {
+    this.afternoon = translate.instant('AFTERNOON');
+    this.morning = translate.instant('MORNING');
+    this.night = translate.instant('NIGHT');
+
+    this.monday = translate.instant('MONDAY_SHORT');
+    this.tuesday = translate.instant('TUESDAY_SHORT');
+    this.wednesday = translate.instant('WEDNESDAY_SHORT');
+    this.thursday = translate.instant('THURSDAY_SHORT');
+    this.friday = translate.instant('FRIDAY_SHORT');
   }
 
   getValue(horario: string): string {
     for (let i in this.disciplinas) {
-      if (this.disciplinas[i].horario.startsWith(horario)){
+      if (this.disciplinas[i].horario.startsWith(horario)) {
         return this.disciplinas[i].local;
       }
     }
     return "";
   }
 
+  onSlideChanged(event: any): void {
+    console.log(event);
+  }
+
+  onSwiped(event: any): void {
+    console.log(event);
+  }
+
   onDisciplinaClicked(event: any) {
-    let text; 
-    if (event.srcElement.id == ''){
+    let text;
+    if (event.srcElement.id == '') {
       text = event.srcElement.innerHTML;
     } else {
       text = event.srcElement.id;
